df6509d41e4c64060278b186bb96a8adf24f7f64
Artem Hordiyevskyy <139376262+slonic32@users.noreply.github.com>
2025-10-11 15:12:16 +0300

    page layout
    add basic page layout
    add service components

---
+++ src/pages/SignInPage/SignInPage.jsx
@@ -0,0 +1,9 @@
+import css from "./SignInPage.module.css";
+
+export default function SignInPage() {
+  return (
+    <main className={css.container}>
+      <h1>Login page</h1>
+    </main>
+  );
+}
