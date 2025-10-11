a6bdbce7ccc77cb017cb028269aa7082e2dbae1d
krasimirevlogiev <evlogievkrasi@gmail.com>
2025-10-11 16:44:20 +0300

    dependency issue resolved after merge

--- src/App.jsx
+++ src/App.jsx
@@ -3,23 +3,15 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import { refresh } from "./redux/auth/operations.js";
 import { store } from "./redux/store.js";
 import { useAuth } from "./hooks/useAuth.js";
-import { useEffect } from "react";
-import { Toaster } from "react-hot-toast";
-
-import SignInPage from "./pages/SignInPage/SignInPage";
-import SignUpPage from "./pages/SignUpPage/SignUpPage";
-
-import { Route, Routes } from "react-router-dom";
-import { Suspense, lazy } from "react";
+import { useEffect, Suspense, lazy } from "react";
 import { useSelector } from "react-redux";
+import { Toaster } from "react-hot-toast";
 
 import { selectError, selectLoading } from "./redux/selectors.js";
 import Loader from "./components/Loader/Loader.jsx";
 import RestrictedRoute from "./components/RestrictedRoute/RestrictedRoute.jsx";
 import PrivateRoute from "./components/PrivateRoute/PrivateRoute.jsx";
 import Error from "./components/Error/Error.jsx";
-
-import { Toaster } from "react-hot-toast";
 
 const HomePage = lazy(() => import("./pages/HomePage/HomePage.jsx"));
 const SignInPage = lazy(() => import("./pages/SignInPage/SignInPage.jsx"));
--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -2,9 +2,9 @@ import { BeatLoader } from "react-spinners";
 import css from "./Loader.module.css";
 
 export default function Loader() {
-  return (
-    <div className={css.loader}>
-      <BeatLoader color="blue" />
-    </div>
-  );
+    return (
+        <div className={css.loader}>
+            <BeatLoader color="blue" />
+        </div>
+    );
 }
