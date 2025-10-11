0fce586ed3fe7f533763d3bcfea44f7e8c4435f6
krasimirevlogiev <evlogievkrasi@gmail.com>
2025-10-11 17:26:39 +0300

    restore app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,26 @@
-import { useDispatch } from "react-redux";
-import { Routes, Route, Navigate } from "react-router-dom";
-import { refresh } from "./redux/auth/operations.js";
-import { store } from "./redux/store.js";
-import { useAuth } from "./hooks/useAuth.js";
-import { useEffect, Suspense, lazy } from "react";
-import { useSelector } from "react-redux";
-import { Toaster } from "react-hot-toast";
+import { Route, Routes } from 'react-router-dom';
+import { Suspense, lazy, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { refresh } from './redux/auth/operations.js';
+import { selectError, selectLoading } from './redux/selectors.js';
+import Loader from './components/Loader/Loader.jsx';
+import RestrictedRoute from './components/RestrictedRoute/RestrictedRoute.jsx';
+import PrivateRoute from './components/PrivateRoute/PrivateRoute.jsx';
+import Error from './components/Error/Error.jsx';
+import { store } from './redux/store.js';
+import { Toaster } from 'react-hot-toast';
 
-import { selectError, selectLoading } from "./redux/selectors.js";
-import Loader from "./components/Loader/Loader.jsx";
-import RestrictedRoute from "./components/RestrictedRoute/RestrictedRoute.jsx";
-import PrivateRoute from "./components/PrivateRoute/PrivateRoute.jsx";
-import Error from "./components/Error/Error.jsx";
+import { useAuth } from './hooks/useAuth.js';
 
-const HomePage = lazy(() => import("./pages/HomePage/HomePage.jsx"));
-const SignInPage = lazy(() => import("./pages/SignInPage/SignInPage.jsx"));
-const SignUpPage = lazy(() => import("./pages/SignUpPage/SignUpPage.jsx"));
-const TrackerPage = lazy(() => import("./pages/TrackerPage/TrackerPage.jsx"));
-const ErrorPage = lazy(() => import("./pages/ErrorPage/ErrorPage.jsx"));
+const HomePage = lazy(() => import('./pages/HomePage/HomePage.jsx'));
+const SignInPage = lazy(() => import('./pages/SignInPage/SignInPage.jsx'));
+const SignUpPage = lazy(() => import('./pages/SignUpPage/SignUpPage.jsx'));
+const TrackerPage = lazy(() => import('./pages/TrackerPage/TrackerPage.jsx'));
+const ErrorPage = lazy(() => import('./pages/ErrorPage/ErrorPage.jsx'));
 
 export default function App() {
   const dispatch = useDispatch();
-  const { isRefreshing, isLoggedIn } = useAuth();
+  const { isRefreshing } = useAuth();
 
   useEffect(() => {
     store.dispatch(refresh());
@@ -33,32 +32,49 @@ export default function App() {
   return isRefreshing ? (
     <b>Refreshing user...</b>
   ) : (
-    <>
-      <Routes>
-        <Route
-          path="/signin"
-          element={
-            isLoggedIn ? <Navigate to="/" replace /> : <SignInPage />
-          }
-        />
-        <Route
-          path="/signup"
-          element={
-            isLoggedIn ? <Navigate to="/" replace /> : <SignUpPage />
-          }
-        />
-        <Route
-          path="/"
-          element={
-            isLoggedIn ? (
-              <h1>Welcome to FitSync Dashboard!</h1>
-            ) : (
-              <Navigate to="/signin" replace />
-            )
-          }
-        />
-      </Routes>
-      <Toaster position="top-right" />
-    </>
+    <div>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <RestrictedRoute redirectTo="/tracker" component={<HomePage />} />
+            }
+          />
+
+          <Route
+            path="/signup"
+            element={
+              <RestrictedRoute
+                redirectTo="/tracker"
+                component={<SignUpPage />}
+              />
+            }
+          />
+
+          <Route
+            path="/signin"
+            element={
+              <RestrictedRoute
+                redirectTo="/tracker"
+                component={<SignInPage />}
+              />
+            }
+          />
+
+          <Route
+            path="/tracker"
+            element={
+              <PrivateRoute redirectTo="/signin" component={<TrackerPage />} />
+            }
+          />
+
+          <Route path="*" element={<ErrorPage />} />
+        </Routes>
+      </Suspense>
+      <Toaster />
+      {loading && <Loader />}
+      {error && <Error />}
+    </div>
   );
 }
