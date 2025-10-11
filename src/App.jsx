bdabe5683bd03934569bd3a7927cfc76b07927b0
krasimirevlogiev <evlogievkrasi@gmail.com>
2025-10-11 17:28:07 +0300

    restore app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,26 @@
-import { Route, Routes } from 'react-router-dom';
-import { Suspense, lazy, useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { refresh } from './redux/auth/operations.js';
-import { selectError, selectLoading } from './redux/selectors.js';
-import Loader from './components/Loader/Loader.jsx';
-import RestrictedRoute from './components/RestrictedRoute/RestrictedRoute.jsx';
-import PrivateRoute from './components/PrivateRoute/PrivateRoute.jsx';
-import Error from './components/Error/Error.jsx';
-import { store } from './redux/store.js';
-import { Toaster } from 'react-hot-toast';
+import { useDispatch } from "react-redux";
+import { refresh } from "./redux/auth/operations.js";
+import { store } from "./redux/store.js";
+import { useAuth } from "./hooks/useAuth.js";
+import { useEffect } from "react";
 
-import { useAuth } from './hooks/useAuth.js';
+import { Route, Routes } from "react-router-dom";
+import { Suspense, lazy } from "react";
+import { useSelector } from "react-redux";
 
-const HomePage = lazy(() => import('./pages/HomePage/HomePage.jsx'));
-const SignInPage = lazy(() => import('./pages/SignInPage/SignInPage.jsx'));
-const SignUpPage = lazy(() => import('./pages/SignUpPage/SignUpPage.jsx'));
-const TrackerPage = lazy(() => import('./pages/TrackerPage/TrackerPage.jsx'));
-const ErrorPage = lazy(() => import('./pages/ErrorPage/ErrorPage.jsx'));
+import { selectError, selectLoading } from "./redux/selectors.js";
+import Loader from "./components/Loader/Loader.jsx";
+import RestrictedRoute from "./components/RestrictedRoute/RestrictedRoute.jsx";
+import PrivateRoute from "./components/PrivateRoute/PrivateRoute.jsx";
+import Error from "./components/Error/Error.jsx";
+
+import { Toaster } from "react-hot-toast";
+
+const HomePage = lazy(() => import("./pages/HomePage/HomePage.jsx"));
+const SignInPage = lazy(() => import("./pages/SignInPage/SignInPage.jsx"));
+const SignUpPage = lazy(() => import("./pages/SignUpPage/SignUpPage.jsx"));
+const TrackerPage = lazy(() => import("./pages/TrackerPage/TrackerPage.jsx"));
+const ErrorPage = lazy(() => import("./pages/ErrorPage/ErrorPage.jsx"));
 
 export default function App() {
   const dispatch = useDispatch();
